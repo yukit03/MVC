9364e4ce594482d4887d92b597d502720b770c0b
Yuki <yuki-tanihata@cal.co.jp>
2023-07-12 09:29:18 +0900

    aaaaa

---
+++ src/main/java/com/example/demo/controller/HelloViewController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("hello")//ブラウザから/helloというリクエストが来た時に呼び出される
+public class HelloViewController {
+	
+	
+	@GetMapping("view")
+	public String helloView() {
+		//戻り値は「ビュー名」を返す
+		return "hello";
+		
+	}
+
+}
